3e6b19da7d1275a8a1785a53d94e7b03a1d74c7b
nc1z <nc1zdev@gmail.com>
2022-11-06 11:54:51 +0800

    momentjs & transaction history

--- slot-machine/main.js
+++ slot-machine/main.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
+import moment from "moment";
 
-// STATE
+// STATE ---------------------->
 let balance = localStorage.getItem("bal");
 if (!balance) {
   balance = 1000;
@@ -9,32 +10,32 @@ let position = 0;
 
 const results = {
   digits: [1, 2, 3],
+  history: [],
 };
 
-// GETTING ELEMENTS
+// GETTING ELEMENTS ---------------------->
 const input = $("#positionSize");
 const playButton = $("#play");
 const balanceDisplay = $("#balance");
-const positionDisplay = $("#position");
 
-// WINDOW ONLOAD
+// WINDOW ONLOAD ---------------------->
 
 $(() => {
   balanceDisplay.text(`Account balance: $${balance}`);
 });
 
-// EVENT LISTENERS
+// EVENT LISTENERS ---------------------->
 
-input.change((e) => {
+input.on("change", (e) => {
   position = Number(e.target.value);
 });
 
-playButton.click(() => {
+playButton.on("click", () => {
+  if (position == 0) {
+    return alert("Please enter a valid amount");
+  }
   if (balance == 0) {
-    alert("You are bankrupt. Game will be reset");
-    balance = 1000;
-    localStorage.setItem("bal", balance);
-    return render();
+    return reset();
   }
   if (balance - position < 0) {
     alert("Insufficient balance");
@@ -47,10 +48,34 @@ playButton.click(() => {
   return render();
 });
 
-// FUNCTIONS
+// FUNCTIONS ---------------------->
 
 const render = () => {
   balanceDisplay.text(`Account balance: $${balance}`);
+
+  $("#history").children().remove();
+
+  if (results.history.length > 0) {
+    results.history.map((n) => {
+      const historyRow = $("<tr>").addClass("tablerow");
+      $("#history").append(historyRow);
+      const history = $("<td>").addClass("tablehistory");
+      const pnl = $("<td>").addClass("tablehistory");
+      historyRow.append(history, pnl);
+      history.text(`${n[1]}`);
+      pnl.text(`${n[0]}`);
+      if (n[0] > 0) {
+        pnl.addClass("gain");
+      } else if (n[0] < 0) {
+        pnl.addClass("loss");
+      }
+      return;
+    });
+  }
+
+  while (results.history.length > 10) {
+    results.history.pop();
+  }
 };
 
 const roll = () => {
@@ -71,11 +96,33 @@ const roll = () => {
   });
 };
 
+const reset = () => {
+  alert("You are bankrupt. Game will be reset");
+  results.history = [];
+  balance = 1000;
+  localStorage.setItem("bal", balance);
+  return render();
+};
+
+// WIN CONDITIONS ---------------------->
 const checkWin = () => {
   if (results.digits.every((n) => n === 7)) {
+    results.history.unshift([
+      position * 9,
+      moment().format("MMM Do YY, h:mm:ss a"),
+    ]);
     balance += position * 10;
   } else if (results.digits.some((n) => n === 7)) {
+    results.history.unshift([
+      position,
+      moment().format("MMM Do YY, h:mm:ss a"),
+    ]);
     balance += position * 2;
+  } else {
+    results.history.unshift([
+      -position,
+      moment().format("MMM Do YY, h:mm:ss a"),
+    ]);
   }
   input.attr("value", 0);
 };
