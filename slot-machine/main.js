6477a04a18ee2ef35674b482de78501b51dbd7ff
nc1z <nc1zdev@gmail.com>
2022-11-06 14:33:38 +0800

    combo desc, complex logic & reset btn

--- slot-machine/main.js
+++ slot-machine/main.js
@@ -18,6 +18,7 @@ const input = $("#positionSize");
 const playButton = $("#play");
 const balanceDisplay = $("#balance");
 const percentButtons = $(".percent");
+const resetButton = $("#reset");
 
 // WINDOW ONLOAD ---------------------->
 
@@ -55,6 +56,10 @@ percentButtons.on("click", (e) => {
   position = amt;
 });
 
+resetButton.on("click", () => {
+  return reset();
+});
+
 // FUNCTIONS ---------------------->
 
 const render = () => {
@@ -69,7 +74,9 @@ const render = () => {
       const history = $("<td>").addClass("tablehistory");
       const combo = $("<td>").addClass("tablehistory");
       const pnl = $("<td>").addClass("tablehistory");
-      historyRow.append(history, combo, pnl);
+      const streak = $("<td>").addClass("tablehistory");
+      historyRow.append(history, combo, pnl, streak);
+      streak.text(`${n[3]}`);
       combo.text(`${n[2]}`);
       history.text(`${n[1]}`);
       pnl.text(`${n[0]}`);
@@ -106,15 +113,34 @@ const roll = () => {
 };
 
 const reset = () => {
-  alert("You are bankrupt. Game will be reset");
+  alert("Game will be reset");
   results.history = [];
   balance = 1000;
   localStorage.setItem("bal", balance);
   return render();
 };
 
+const isStraight = (arr) => {
+  let res = true;
+  if (Math.min(arr) >= 0) {
+    for (let i = 1; i < arr.length; i++) {
+      if (res) {
+        res = arr[i] - arr[i - 1] == 1;
+      }
+    }
+  } else {
+    for (let i = 1; i < arr.length; i++) {
+      if (res) {
+        res = arr[i] - arr[i - 1] == -1;
+      }
+    }
+  }
+  return res;
+};
+
 // WIN CONDITIONS ---------------------->
 const checkWin = () => {
+  // Storing and mapping out count of each result
   const counter = {
     0: 0,
     1: 0,
@@ -132,26 +158,87 @@ const checkWin = () => {
     counter[n] += 1;
   });
 
-  if (results.digits.every((n) => n === 7)) {
+  const keys = Object.keys(counter);
+  const values = keys.map((key) => {
+    return counter[key];
+  });
+  const maxVal = Math.max.apply(null, values);
+
+  // Check Gains & Losses
+  if (isStraight(results.digits)) {
+    // COMBO [6X]
+    results.history.unshift([
+      position * 5,
+      moment().format("h:mm:ss a"),
+      results.digits,
+      "COMBO[6X]",
+    ]);
+    balance += position * 6;
+    alert("Combo!");
+  } else if (counter[7] == 3) {
+    // JACKPOT 777 [20X]
     results.history.unshift([
       position * 19,
-      moment().format("MMM Do YY, h:mm:ss a"),
+      moment().format("h:mm:ss a"),
       results.digits,
+      "JACKPOT[20X]",
     ]);
     balance += position * 10;
     alert("JACKPOT!");
   } else if (counter[7] > 0) {
+    if (counter[7] == 2) {
+      // NUMBER 7 PAIR [6X]
+      results.history.unshift([
+        position * 5,
+        moment().format("h:mm:ss a"),
+        results.digits,
+        "7PAIR[6X]",
+      ]);
+      balance += position * 6;
+    } else if (counter[7] == 1 && maxVal > 1) {
+      // NUMBER 7 + OTHER PAIR [4X]
+      results.history.unshift([
+        position * 3,
+        moment().format("h:mm:ss a"),
+        results.digits,
+        "PAIRWITH7[4X]",
+      ]);
+      balance += position * 4;
+    } else {
+      // NUMBER 7 ONCE [3X]
+      results.history.unshift([
+        position * 2,
+        moment().format("h:mm:ss a"),
+        results.digits,
+        "7ONCE[3X]",
+      ]);
+      balance += position * 3;
+    }
+  } else if (maxVal == 3) {
+    // ANY NUMBER THRICE [8X]
     results.history.unshift([
-      position,
-      moment().format("MMM Do YY, h:mm:ss a"),
+      position * 7,
+      moment().format("h:mm:ss a"),
       results.digits,
+      "TRIPLE[8X]",
     ]);
-    balance += position * 2;
+    balance += position * 8;
+  } else if (maxVal > 1) {
+    // ANY NUMBER TWICE [3X]
+    results.history.unshift([
+      position * 2,
+      moment().format("h:mm:ss a"),
+      results.digits,
+      "DOUBLE[3X]",
+    ]);
+    balance += position * 3;
   } else {
+    // LOSS
     results.history.unshift([
       -position,
-      moment().format("MMM Do YY, h:mm:ss a"),
+      moment().format("h:mm:ss a"),
       results.digits,
+      "LOSS[-1X]",
     ]);
   }
 };
